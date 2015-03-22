165823fc4d33aa4704f3eed5ef81aa684a669423
bkicia <bkicia@vt.edu>
2015-03-22 19:10:54 -0400

    Update User.cs

--- User.cs
+++ User.cs
@@ -14,70 +14,84 @@ namespace UserSpace
 	{
 		/**Variables that hold the user's information**/
 		private string username, /**< The username */
-			       password; /**< The user's password */
-		private string[] settings /**< The user's preferred system settings */
+					   password; /**< The user's password */
+		private string[] settings; /**< The user's preferred system settings */
 					
 		/** A constructor.
 		* @param username is the value that will set the internal username
 		* @param password is the value that will set the internal password
 		**/
 		User(string username, string password){
-			updateUsernameAndPassword (username, password);
+			updateUsername(username);
+			updatePassword(password);
 		}
 
 		User(){
-			updateUsernameAndPassword (String.Empty, String.Empty);
+			updateUsername(string.Empty);
+			updatePassword(string.Empty);
 		}
 
 		/**A function that takes no arguments and returns the username and password.
 			@return The username, password, and other user information
 			*/
 		string[] getInfo(){
-			return new string[3]; //return username, password, user info
+			return new string[5 /*number of settings plus 2*/]; //return username and password appended to the user info array
 		}
 
-		/**A function that updates the internal login information
+		/**A function that updates the internal login username
 		* @param username is the value that will set the internal username
+		**/
+		void updateUsername(string username){
+			this.username = username;
+		}
+
+		/**A function that updates the internal login password
 		* @param password is the value that will set the internal password
 		**/
-		void updateUsernameAndPassword(string username, string password){
-			this->username = username;
-			this->password = password;
+		void updatePassword(string password){
+			this.password = password;
 		}
 
 		/**A function that updates the internal user settings
 		* @param settings is the array that will be the user's settings
 		**/
 		void updateSettings(string[] settings){
-			this->settings = settings;
+			this.settings = settings;
 		}
 
 		/**A function that returns the username
 		 * @return username
 		 * */
 		string getUsername(){
-			return username;
+			return this.username;
 		}
 
 		/**A function that returns the user's password
 		 * @return password
 		 * */
 		string getPassword(){
-			return password;
+			return this.password;
 		}
 
 		/**A function that returns the user's settings
 		 * @return settings
 		 * */
 		string[] getSettings(){
-			return settings;
+			return this.settings;
+		}
+
+		/**A function that creates a new user in the database.
+		 * */
+		bool createUser(){
+			return true;
 		}
 
 		/**A function that updates the user's information
 		 * on the database.
 		 * */
-		void updateUser(){
+		bool updateUser(){
+			return true;
 		}
-		
+
 	}
 }
